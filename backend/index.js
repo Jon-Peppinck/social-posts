91588b00b3127c5781bcd2cc71e9dc028516faed
Jon-Peppinck <mrjonpeppinck@gmail.com>
2020-09-07 12:54:43 +0700

    Access-Control-Allow-Headers - Options

--- backend/index.js
+++ backend/index.js
@@ -16,8 +16,17 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, OPTIONS'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Accept, X-Custom-Header, Authorization'
+  );
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
   next();
 });
 
